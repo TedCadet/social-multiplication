a0a5820a6786b245a9004f2b64fd6c7c5d12b51d
TedCadet <e_cadet22@hotmail.com>
2023-02-22 22:56:59 -0500

    checkAttempt added to the MultiplicationService interface added simple implementation of MultiplicationService added MultiplicationServiceTest modified to pass

--- src/main/java/microservices/book/multiplication/service/MultiplicationService.java
+++ src/main/java/microservices/book/multiplication/service/MultiplicationService.java
@@ -1,11 +1,19 @@
 package microservices.book.multiplication.service;
 
 import microservices.book.multiplication.domain.Multiplication;
+import microservices.book.multiplication.domain.MultiplicationResultAttempt;
 
 public interface MultiplicationService {
 /**
- * Creates a Multiplication object with two randomly-generated factors between 11 and 99
+ * Creates a {@link Multiplication} object with two randomly-generated factors between 11 and 99
  * @return a multiplication object with random factors
  */
     Multiplication createRandomMultiplication();
+
+    /**
+     *
+     * @param resultAttempt
+     * @return true if the attempt matches the result of the multiplication, false otherwise.
+     */
+    boolean checkAttempt(final MultiplicationResultAttempt resultAttempt);
 }
--- src/test/java/microservices/book/multiplication/service/MultiplicationServiceTest.java
+++ src/test/java/microservices/book/multiplication/service/MultiplicationServiceTest.java
@@ -3,14 +3,9 @@ package microservices.book.multiplication.service;
 import microservices.book.multiplication.domain.Multiplication;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
-import org.junit.jupiter.params.provider.ValueSource;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
@@ -19,23 +14,21 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.BDDMockito.given;
 
 @ExtendWith(SpringExtension.class)
-@SpringBootTest
 @DisplayName("Given a multiplication service")
-public class MultiplicationServiceTest {
+class MultiplicationServiceTest {
     //int rand;
     Multiplication multiplication;
 
     @MockBean
     private RandomGeneratorService randomGeneratorService;
 
-    @Autowired
     private MultiplicationService multiplicationService;
 
     @BeforeEach
-    @DisplayName("When multiplication service creates a random multiplication")
-    public void givenRGS() {
-        //rand = randomGeneratorService.generateRandomFactor();
-        multiplication = multiplicationService.createRandomMultiplication();
+    @DisplayName("Setting up before the tests")
+    public void setUp() {
+        multiplicationService = new MultiplicationServiceImpl(randomGeneratorService);
+
     }
 
     @ParameterizedTest
@@ -43,14 +36,15 @@ public class MultiplicationServiceTest {
     @CsvSource({
             "50,30,1500"
     })
-    public void createRandomMultiplicationTest(int factorA, int factorB, int result) {
+    void createRandomMultiplicationTest(int factorA, int factorB, int result) {
         // given our mocked Random Generator service will return first 50, then 30
-        given(randomGeneratorService.generateRandomFactor()).willReturn(50, 30);
+        given(randomGeneratorService.generateRandomFactor()).willReturn(factorA, factorB);
+        multiplication = multiplicationService.createRandomMultiplication();
 
         assertAll(
-                () -> assertEquals(multiplication.getFactorA(), factorA),
-                () -> assertEquals(multiplication.getFactorB(), factorB),
-                () -> assertEquals(multiplication.getResult(), result)
+                () -> assertEquals(factorA, multiplication.getFactorA()),
+                () -> assertEquals(factorB, multiplication.getFactorB()),
+                () -> assertEquals(result, multiplication.getResult())
         );
     }
 }
