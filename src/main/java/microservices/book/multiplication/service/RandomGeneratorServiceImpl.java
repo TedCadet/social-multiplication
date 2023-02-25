7fb92968d2220cad46a59ac0fdbbdf7fc8a26629
TedCadet <e_cadet22@hotmail.com>
2023-02-25 17:44:09 -0500

    randomGeneratorService.generateRandomFactor() implemented

--- src/main/java/microservices/book/multiplication/service/RandomGeneratorServiceImpl.java
+++ src/main/java/microservices/book/multiplication/service/RandomGeneratorServiceImpl.java
@@ -1,12 +1,15 @@
 package microservices.book.multiplication.service;
 
+import java.util.Random;
 import org.springframework.stereotype.Service;
 
 @Service
 public class RandomGeneratorServiceImpl implements RandomGeneratorService{
 
+  private static final int MINIMUM_FACTOR = 11;
+  private static final int MAXIMUM_FACTOR = 99;
   @Override
   public int generateRandomFactor() {
-    return 0;
+    return new Random().nextInt((MAXIMUM_FACTOR - MINIMUM_FACTOR) + 1) + MINIMUM_FACTOR;
   }
 }
