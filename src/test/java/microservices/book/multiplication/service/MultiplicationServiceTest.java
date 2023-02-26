84bfbd75f8e0a3ec23e107bb1e1e31d3d44a15bb
TedCadet <e_cadet22@hotmail.com>
2023-02-25 19:14:28 -0500

    Mockbean -> Mock

--- src/test/java/microservices/book/multiplication/service/MultiplicationServiceTest.java
+++ src/test/java/microservices/book/multiplication/service/MultiplicationServiceTest.java
@@ -1,50 +1,51 @@
 package microservices.book.multiplication.service;
 
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.BDDMockito.given;
+
 import microservices.book.multiplication.domain.Multiplication;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.mockito.Mock;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-
-import static org.junit.jupiter.api.Assertions.assertAll;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.BDDMockito.given;
 
 @ExtendWith(SpringExtension.class)
 @DisplayName("Given a multiplication service")
 class MultiplicationServiceTest {
-    //int rand;
-    Multiplication multiplication;
 
-    @MockBean
-    private RandomGeneratorService randomGeneratorService;
+  //int rand;
+  Multiplication multiplication;
 
-    private MultiplicationService multiplicationService;
+  @Mock
+  private RandomGeneratorService randomGeneratorService;
 
-    @BeforeEach
-    @DisplayName("Setting up before the tests")
-    public void setUp() {
-        multiplicationService = new MultiplicationServiceImpl(randomGeneratorService);
+  private MultiplicationService multiplicationService;
 
-    }
+  @BeforeEach
+  @DisplayName("Setting up before the tests")
+  public void setUp() {
+    multiplicationService = new MultiplicationServiceImpl(randomGeneratorService);
 
-    @ParameterizedTest
-    @DisplayName("Then assert that factor A and factor b is equals to 30, 50 and the result is equals to 1500")
-    @CsvSource({
-            "50,30,1500"
-    })
-    void createRandomMultiplicationTest(int factorA, int factorB, int result) {
-        // given our mocked Random Generator service will return first 50, then 30
-        given(randomGeneratorService.generateRandomFactor()).willReturn(factorA, factorB);
-        multiplication = multiplicationService.createRandomMultiplication();
+  }
 
-        assertAll(
-                () -> assertEquals(factorA, multiplication.getFactorA()),
-                () -> assertEquals(factorB, multiplication.getFactorB()),
-                () -> assertEquals(result, multiplication.getResult())
-        );
-    }
+  @ParameterizedTest
+  @DisplayName("Then assert that factor A and factor b is equals to 30, 50 and the result is equals to 1500")
+  @CsvSource({
+      "50,30,1500"
+  })
+  void createRandomMultiplicationTest(int factorA, int factorB, int result) {
+    // given our mocked Random Generator service will return first 50, then 30
+    given(randomGeneratorService.generateRandomFactor()).willReturn(factorA, factorB);
+    multiplication = multiplicationService.createRandomMultiplication();
+
+    assertAll(
+        () -> assertEquals(factorA, multiplication.getFactorA()),
+        () -> assertEquals(factorB, multiplication.getFactorB()),
+        () -> assertEquals(result, multiplication.getResult())
+    );
+  }
 }
